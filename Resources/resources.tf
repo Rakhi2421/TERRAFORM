341e367bb7393fd9a5734c9e5d61ea3b200c26b2
Rakhi2421 <srirakesh242001@gmail.com>
2025-12-16 09:06:34 +0530

    tags used in resources

--- Resources/resources.tf
+++ Resources/resources.tf
@@ -7,3 +7,23 @@ resource "aws_subnet" "learning-subnet" {
     cidr_block = "10.0.10.0/24"
     availability_zone = "eu-west-3a"  
 }
+
+# Using tags- Tags are key value pairs in aws.
+# syntax : mykey: myvalue 
+
+resource "aws_vpc" "learning-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name: "development"
+        vpc_env: "dev"
+    }
+}
+resource "aws_subnet" "learning-subnet" {
+    vpc_id = aws_vpc.learning-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "eu-west-3a" 
+    tags = {
+        Name: "learning-subnet-1"
+    } 
+}
+
